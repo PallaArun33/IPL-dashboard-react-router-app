b78c68824f294baa69f22a5e2b2b3dbb11a3513c
arun <parun00007.com>
2023-10-30 11:03:02 +0000

    ipl match

---
+++ src/components/Home/index.js
@@ -0,0 +1,63 @@
+// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TeamCard from '../TeamCard'
+import './index.css'
+
+class Home extends Component {
+  state = {iplTeamsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getIplTeamsData()
+  }
+
+  getIplTeamsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = await response.json()
+    console.log(data)
+    const updatedData = data.teams.map(eachTeam => ({
+      name: eachTeam.name,
+      id: eachTeam.id,
+      teamImageUrl: eachTeam.team_image_url,
+    }))
+    this.setState({iplTeamsData: updatedData, isLoading: false})
+  }
+
+  renderTeamCard = () => {
+    const {iplTeamsData} = this.state
+
+    return (
+      <ul className="ul-items">
+        {iplTeamsData.map(item => (
+          <TeamCard key={item.id} teamCardDetails={item} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderLoading = () => (
+    <div className="loader-container" data-testid="loader">
+      <Loader type="Oval" color="#ffffff" height={50} />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="ipl-logo-heading">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+            alt="ipl logo"
+            className="ipl-logo"
+          />
+          <h1 className="ipl-heading">IPL Dashboard</h1>
+        </div>
+        {isLoading ? this.renderLoading() : this.renderTeamCard()}
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {teamCardDetails} = props
+  const {id, name, teamImageUrl} = teamCardDetails
+
+  return (
+    <li className="list-items">
+      <Link className="team-item-link" to={`/team-matches/${id}`}>
+        <img src={teamImageUrl} alt={name} className="team-image" />
+        <p className="team-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+export default TeamCard
